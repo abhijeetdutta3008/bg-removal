1e44fe6216bd1e0bf6dfb28efb5e65315a199fd2
Abhijeet Dutta <93587547+abhijeetdutta3008@users.noreply.github.com>
2024-12-21 22:00:58 +0530

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -5,11 +5,11 @@ import razorpay from 'razorpay';
 import stripe from "stripe";
 
 // Gateway Initialize
-const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
-const razorpayInstance = new razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-})
+// const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
+// const razorpayInstance = new razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET,
+// })
 
 // API Controller Function to Manage Clerk User with database
 const clerkWebhooks = async (req, res) => {
